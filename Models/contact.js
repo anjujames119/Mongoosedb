114c3253f1384e7f26addf0ab83b7bd68b4a0cb6
anjujames119 <anjujames119@gmail.com>
2018-11-23 15:50:44 +0530

    first commit

---
+++ Models/contact.js
@@ -0,0 +1,96 @@
+var express = require('express');
+//var JSONData = require('./heros.json');
+//var fs = require("fs");
+var mongoose = require('mongoose');
+    
+let Contact= {}
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+ 
+const contactDB = new Schema({
+  Contact: ObjectId,
+  name: String,
+  phoneNo: String
+  
+});
+
+const contactModel = mongoose.model('contact',contactDB);
+
+Contact.getAll = function(){
+	return new Promise (function (resolve, reject){
+	//create the connection to database
+	const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+	console.log(connection);
+	
+	//querying the database for results
+	contactModel.find({},function(err,contactlist){
+		if(err){
+			console.log(err);
+			console.log('ERR :: fetching data from database.');
+			reject();
+		}
+		else{
+			//console.log(result);
+			//console.log(fields);
+			resolve(contactlist);
+		}
+	    });
+    });
+
+}
+
+Contact.saveNew = function(newData){
+   // create the connection to database
+   return new Promise(function(resolve,reject){
+  const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+  console.log(connection);
+	
+	var saveContact =contactModel({
+		name:`${newData.name}`,
+		phoneNo: `${newData.phoneNo}`
+	});
+
+	//console.log(query);
+	saveContact.save({},function(err,results,fields){
+
+		if(err){
+			console.log(err);
+			console.log('ERR: fetching data from database');
+			reject();
+		}
+		else{
+			resolve();
+		}
+	});
+
+   });
+}
+/*
+
+Contact.deletecontact = function(deleteData){
+	return new Promise(function(resolve,reject){
+		 const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+  console.log(connection);
+	
+	var saveContact =contactModel({
+		name:`${newData.name}`,
+		phoneNo: `${newData.phoneNo}`
+	});
+		
+		 connection.query(query,function(err,result,fields){
+		if(err){
+			console.log(err);
+			console.log('ERR: fetching data from database');
+			reject();
+		}
+		else{
+			resolve(result);
+		}
+	});
+
+	});
+}
+*/
+
+module.exports = Contact;
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var Contacts = require('../models/contact.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Contacts' });
+});
+
+
+router.get('/saveData', function(req, res, next) {
+	  Contacts.saveNew(req.query)
+  	.then (function(){
+  res.redirect('/getAllContacts');
+	})
+	.catch(console.log('ERR : in resolving the promise'))
+
+
+});
+
+
+router.get('/getAllContacts', function(req, res, next) {
+   	Contacts.getAll()
+	.then (function(retVal){
+  res.render('contacts', {data: retVal});
+})
+	.catch(console.log('ERR :: in resolving the promise'))
+});
+
+
+module.exports = router;
